ca35f5d4a3e6076b5c2cf47be283999487f7ccdd
davidmaamoaix <davidmaamoaix@gmail.com>
2024-02-14 22:31:09 -0500

    removed build scripts; moving to Makefile

--- build.sh
+++
@@ -1,1 +0,0 @@
-cc0 -d -w -o ccc0 lib/vector.c1 lib/regex.c1 lib/ordered_map.c1 src/utils.c1 src/file_io.c1 src/ast.c1 src/ast_utils.c1 src/file_repr.c1 src/error.c1 src/lexer.c1 src/type_check.c1 src/bc0_format.c1 src/ir_ssa.c1 src/control_flow.c1 src/parser.c1 src/compile.c1 src/main.c1
